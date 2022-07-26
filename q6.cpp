ce3591dc466b005d50a8e62e4f96b2bda824ca63
taha-hme <tahahashemi463@gmail.com>
2022-07-27 01:25:55 +0430

    Create q6.cpp

---
+++ q6.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <bitset>
+#include <cmath>
+using namespace std;
+int main()
+{
+    int n, i, s = 0;
+    cout << "enter digit number: ";
+    cin >> n;
+    for (i = round(pow(2, n - 1)); i < round(pow(2, n)); i++)
+        if ((bitset<24>(i).to_string().find("00000", 24 - n) != string::npos) || (bitset<24>(i).to_string().find("11111", 24 - n) != string::npos))
+            s += 1;
+    cout << s;
+}
