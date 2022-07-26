b1d9485fbe889d3a2f2280091590e77f43a8a6e7
taha-hme <tahahashemi463@gmail.com>
2022-07-26 17:29:59 +0430

    Update q2.cpp

--- q2.cpp
+++ q2.cpp
@@ -1,5 +1,5 @@
-#include<iostream>
-#include<algorithm>
+#include <iostream>
+#include <algorithm>
 using namespace std;
 string findSum(string str1, string str2)
 {
@@ -9,22 +9,20 @@ string findSum(string str1, string str2)
     int n1 = str1.length(), n2 = str2.length();
     int diff = n2 - n1;
     int carry = 0;
-    for (int i=n1-1; i>=0; i--)
+    for (int i = n1 - 1; i >= 0; i--)
     {
-        int sum = ((str1[i]-'0') +
-                   (str2[i+diff]-'0') +
-                   carry);
-        str.push_back(sum%10 + '0');
-        carry = sum/10;
+        int sum = ((str1[i] - '0') + (str2[i + diff] - '0') + carry);
+        str.push_back(sum % 10 + '0');
+        carry = sum / 10;
     }
-    for (int i=n2-n1-1; i>=0; i--)
+    for (int i = n2 - n1 - 1; i >= 0; i--)
     {
-        int sum = ((str2[i]-'0')+carry);
-        str.push_back(sum%10 + '0');
-        carry = sum/10;
+        int sum = ((str2[i] - '0') + carry);
+        str.push_back(sum % 10 + '0');
+        carry = sum / 10;
     }
     if (carry)
-        str.push_back(carry+'0');
+        str.push_back(carry + '0');
     reverse(str.begin(), str.end());
     return str;
 }
