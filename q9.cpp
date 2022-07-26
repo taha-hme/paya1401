4eb0c18297982a2343a7b99747b6ce88f2f77d7a
taha-hme <tahahashemi463@gmail.com>
2022-07-26 23:09:41 +0430

    Create q9.cpp

---
+++ q9.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
+int main()
+{
+    int n, i;
+    string str1, str2;
+    cout << "enter the number: ";
+    cin >> n;
+    str2 = to_string(n);
+    for (i = 2; i < 1000000; i++)
+    {
+        if (log10(i) == floor(log10(i)))
+            continue;
+        str1 = to_string(n * i);
+        if (str1.find(str2) != string::npos)
+        {
+            cout << n * i;
+            break;
+        }
+        else
+            continue;
+    }
+    return 0;
+}
